ba64e26a2178f0bf8d859191aca4c9ce0f59b07f
Peter Goetz <peter.gtz@gmail.com>
2018-01-24 23:42:33 +0100

    Add task update-cloud-config

---
+++ tasks/update-cloud-config.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -ex
+
+pushd state/environments/softlayer/director/$BOSH_LITE_NAME
+    cat hosts >> /etc/hosts
+
+    bosh2 alias-env my-env -e $(cat hosts | cut -f 2 -d ' ') --ca-cert <(bosh2 int ./vars.yml --path /director_ssl/ca)
+    export BOSH_CLIENT=admin
+    export BOSH_CLIENT_SECRET=`bosh2 int ./vars.yml --path /admin_password`
+popd
+
+bosh2 -n -e my-env update-cloud-config cf-deployment/iaas-support/bosh-lite/cloud-config.yml
