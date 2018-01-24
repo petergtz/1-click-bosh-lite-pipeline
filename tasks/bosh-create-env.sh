1cf523590fb6aec194c5f39032b45d4c9f73debb
Peter Goetz <peter.gtz@gmail.com>
2018-01-24 22:49:37 +0100

    Add task bosh-create-env

---
+++ tasks/bosh-create-env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -ex
+
+sudo bosh2 create-env \
+    --state state/environments/softlayer/director/$BOSH_LITE_NAME/state.json \
+    --vars-store=state/environments/softlayer/director/$BOSH_LITE_NAME/vars.yml \
+    $MANIFEST
+
+tail -n1 /etc/hosts > state/environments/softlayer/director/$BOSH_LITE_NAME/hosts
+
+REPO_DIR=state \
+    FILENAME="state/environments/softlayer/director/$BOSH_LITE_NAME/state.json state/environments/softlayer/director/$BOSH_LITE_NAME/vars.yml state/environments/softlayer/director/$BOSH_LITE_NAME/hosts" \
+    COMMIT_MESSAGE="Update state for environments/softlayer/director/$BOSH_LITE_NAME" tasks/commit-if-changed.sh
+
+cp -a state/. out-state
