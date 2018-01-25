977607df54c15382dbc0b0784960cac69c98690b
Peter Goetz <peter.gtz@gmail.com>
2018-01-25 13:44:48 +0100

    Refactor other tasks with bosh-login

--- tasks/bosh-deploy.sh
+++ tasks/bosh-deploy.sh
@@ -1,12 +1,7 @@
 #!/bin/bash -ex
 
-pushd state/environments/softlayer/director/$BOSH_LITE_NAME
-    cat hosts >> /etc/hosts
 
-    bosh2 alias-env my-env -e $(cat hosts | cut -f 2 -d ' ') --ca-cert <(bosh2 int ./vars.yml --path /director_ssl/ca)
-    export BOSH_CLIENT=admin
-    export BOSH_CLIENT_SECRET=`bosh2 int ./vars.yml --path /admin_password`
-popd
+. 1-click/tasks/bosh-login.sh
 
-bosh2 -e my-env upload-stemcell stemcell/stemcell.tgz
-bosh2 -n -e my-env -d cf deploy manifest/manifest.yml --no-redact
+bosh2 upload-stemcell stemcell/stemcell.tgz
+bosh2 -n -d cf deploy manifest/manifest.yml --no-redact
--- tasks/update-cloud-config.sh
+++ tasks/update-cloud-config.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -ex
 
-pushd state/environments/softlayer/director/$BOSH_LITE_NAME
-    cat hosts >> /etc/hosts
+. 1-click/tasks/bosh-login.sh
 
-    bosh2 alias-env my-env -e $(cat hosts | cut -f 2 -d ' ') --ca-cert <(bosh2 int ./vars.yml --path /director_ssl/ca)
-    export BOSH_CLIENT=admin
-    export BOSH_CLIENT_SECRET=`bosh2 int ./vars.yml --path /admin_password`
-popd
-
-bosh2 -n -e my-env update-cloud-config cf-deployment/iaas-support/bosh-lite/cloud-config.yml
+bosh2 -n update-cloud-config cf-deployment/iaas-support/bosh-lite/cloud-config.yml
