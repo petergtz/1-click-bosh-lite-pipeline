1f20f02c46a3d3005a193c5411e3de8e5350cb44
Peter Goetz <peter.gtz@gmail.com>
2018-01-25 09:41:10 +0100

    Properly commit new files

--- tasks/commit-if-changed.sh
+++ tasks/commit-if-changed.sh
@@ -1,24 +1,22 @@
 #!/bin/bash -ex
 
+git config --global user.name "Pipeline"
+git config --global user.email flintstone@cloudfoundry.org
+
 if [[ -z "${REPO_DIR}" ]] || [[ -z "${FILENAME}" ]] || [[ -z "${COMMIT_MESSAGE}" ]]; then
   echo "One of the required parameters is missing!"
   exit 1
 fi
 
 pushd "${REPO_DIR}"
+  git add $FILENAME
   set +e
-    git diff --exit-code -- $FILENAME
-    exit_code=$?
+    git --no-pager diff --cached --exit-code
+    HAS_CHANGES=$?
   set -e
-
-  if [[ $exit_code -eq 0 ]]; then
-    echo "There are no changes to commit."
-  else
-    git config --global user.name "Pipeline"
-    git config --global user.email flintstone@cloudfoundry.org
-
-    git add $FILENAME
-    git --no-pager diff --cached
+  if [[ $HAS_CHANGES -eq 1 ]]; then
     git commit -m "${COMMIT_MESSAGE}"
+  else
+    echo "Nothing to commit"
   fi
 popd
