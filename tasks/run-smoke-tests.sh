a777324d4e725e7f327c3ef08f113bbc5dea154d
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-01-26 16:36:28 +0100

    Add task run-smoke-tests
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

---
+++ tasks/run-smoke-tests.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -ex
+
+mkdir -p gopath/src/github.com/cloudfoundry
+cp -a cf-smoke-tests gopath/src/github.com/cloudfoundry/
+export GOPATH=$PWD/gopath
+export PATH=$PATH:$GOPATH/bin
+
+cf version
+
+cf_admin_password=$(bosh2 int state/environments/softlayer/director/$BOSH_LITE_NAME/cf-deployment/vars.yml --path /cf_admin_password)
+cat > config.json <<EOF
+{
+  "suite_name"                      : "CF_SMOKE_TESTS",
+  "api"                             : "api.${CF_SYSTEM_DOMAIN}",
+  "apps_domain"                     : "${CF_SYSTEM_DOMAIN}",
+  "user"                            : "admin",
+  "password"                        : "${cf_admin_password}",
+  "cleanup"                         : false,
+  "use_existing_org"                : true,
+  "org"                             : "system",
+  "use_existing_space"              : false,
+  "logging_app"                     : "",
+  "runtime_app"                     : "",
+  "enable_windows_tests"            : false,
+  "windows_stack"                   : "windows2012R2",
+  "enable_etcd_cluster_check_tests" : false,
+  "etcd_ip_address"                 : "",
+  "backend"                         : "diego",
+  "isolation_segment_name"          : "is1",
+  "isolation_segment_domain"        : "is1.bosh-lite.com",
+  "enable_isolation_segment_tests"  : false,
+  "skip_ssl_validation"             : true
+}
+EOF
+export CONFIG="$(readlink -nf config.json)"
+
+cd gopath/src/github.com/cloudfoundry/cf-smoke-tests
+
+bin/test -v -r -slowSpecThreshold=120 -randomizeAllSpecs -nodes="${NODES}" -keepGoing
