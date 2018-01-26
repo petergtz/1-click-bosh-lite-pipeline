e3672638245c6ed33b92ec09e629e56115235460
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-01-26 10:49:24 +0100

    Make bosh-cancel-task lenient on missing tasks
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

--- tasks/bosh-cancel-task.sh
+++ tasks/bosh-cancel-task.sh
@@ -2,4 +2,10 @@
 
 . 1-click/tasks/bosh-login.sh
 
-bosh2 cancel-task $(bosh2 -d $DEPLOYMENT_NAME tasks | cut -f 1)
+TASK_ID=$(bosh2 -d $DEPLOYMENT_NAME tasks | cut -f 1)
+
+if [ $TASK_ID ]; then
+    bosh2 cancel-task $(bosh2 -d $DEPLOYMENT_NAME tasks | cut -f 1)
+else
+    echo "No tasks running."
+fi
