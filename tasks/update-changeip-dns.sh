249e0e040d4edff038cdefe9996462452f43b666
Sarah-Denise Schmitz <sarah-denise.schmitz@de.ibm.com>
2018-01-29 14:14:28 +0100

    Add task update-changeip-dns
    Signed-off-by: Peter Goetz <peter.gtz@gmail.com>

---
+++ tasks/update-changeip-dns.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -ex
+
+. 1-click/tasks/bosh-login.sh
+
+bosh2 -d cf instances > instances
+
+router_ip=$(cat state/environments/softlayer/director/$BOSH_LITE_NAME/hosts | cut -f 1 -d ' ')
+
+echo "Make sure the following DNS entry exists:"
+echo "hostname *.$CF_SYSTEM_DOMAIN -> $router_ip"
+
+set +e
+nslookup api.$CF_SYSTEM_DOMAIN | grep "$router_ip"
+EXIT_CODE=$?
+set -e
+until [[ $EXIT_CODE -eq 0 ]]; do
+    echo "Waiting for you to make the change..."
+    sleep 10
+    set +e
+    nslookup api.$CF_SYSTEM_DOMAIN | grep "$router_ip"
+    EXIT_CODE=$?
+    set -e
+done
