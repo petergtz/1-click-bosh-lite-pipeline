1f5c99d44ec149a1c83efd3dab153e9982d4c229
Peter Goetz <peter.gtz@gmail.com>
2018-01-31 14:36:28 +0100

    Run system_domain check simultaneously with deployment, refactor update-changeip-dns

--- tasks/update-changeip-dns.sh
+++ tasks/update-changeip-dns.sh
@@ -1,23 +1,19 @@
 #!/bin/bash -e
 
-. 1-click/tasks/bosh-login.sh
-
-bosh2 -d cf instances > instances
-
-router_ip=$(cat state/environments/softlayer/director/$BOSH_LITE_NAME/hosts | cut -f 1 -d ' ')
+IP=$(cat state/environments/softlayer/director/$BOSH_LITE_NAME/hosts | cut -f 1 -d ' ')
 
 echo "Make sure the following DNS entry exists:"
-echo "hostname *.$CF_SYSTEM_DOMAIN -> $router_ip"
+echo "hostname *.$CF_SYSTEM_DOMAIN -> $IP"
 
 set +e
-nslookup api.$CF_SYSTEM_DOMAIN | grep "$router_ip"
+nslookup api.$CF_SYSTEM_DOMAIN | grep "$IP"
 EXIT_CODE=$?
 set -e
 until [[ $EXIT_CODE -eq 0 ]]; do
     echo "Waiting for you to make the change..."
     sleep 10
     set +e
-    nslookup api.$CF_SYSTEM_DOMAIN | grep "$router_ip"
+    nslookup api.$CF_SYSTEM_DOMAIN | grep "$IP"
     EXIT_CODE=$?
     set -e
 done
