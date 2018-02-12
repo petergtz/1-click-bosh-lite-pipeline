bc2f038641efede3d10db02a3e939813da4872ce
Peter Goetz <peter.gtz@gmail.com>
2018-02-12 15:52:39 +0100

    Fix commit when deleting a VM

--- tasks/bosh-delete-env.sh
+++ tasks/bosh-delete-env.sh
@@ -19,7 +19,8 @@ pushd state/environments/softlayer/director/$BOSH_LITE_NAME
         /tmp/bosh.yml \
         -v director_vm_prefix=$BOSH_LITE_NAME
 
-    git rm state.json vars.yml hosts
+    git rm vars.yml hosts
+    git add state.json
     $commit_if_changed "Update state for environments/softlayer/director/$BOSH_LITE_NAME"
 popd
 
