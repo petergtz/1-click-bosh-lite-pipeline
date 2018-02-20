268d6d2820cad6fa67fb5529b34da489cf31d1e7
Steffen Uhlig <Steffen.Uhlig@de.ibm.com>
2018-02-20 14:00:45 +0100

    Add stemcell detection task
    We would like to use compiled releases, but its stemcell version is not always the latest published version. The new task parses the compiled-releases ops file in order to find out which stemcell version to use.
    
    [#154842976]
    
    Signed-off-by: Norman Sutorius <norman.sutorius@de.ibm.com>

---
+++ tasks/detect-stemcell.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+
+ops_file = YAML.load_file('cf-deployment/operations/use-compiled-releases.yml')
+
+versions = ops_file
+           .select { |op| op['path'] =~ %r(/releases/name=.+/stemcell) }
+           .map { |op| op['value']['version'] }
+
+unique_versions_count = versions.uniq.size
+
+if unique_versions_count == 0
+  warn "Could not find a stemcell version."
+  exit 1
+elsif unique_versions_count > 1
+  warn "Found #{unique_versions_count} unique stemcell versions. Don't know which one to take."
+  exit 2
+end
+
+stemcell_version = "https://bosh.io/d/stemcells/bosh-warden-boshlite-ubuntu-trusty-go_agent?v=#{versions.first}"
+puts stemcell_version
+File.write('stemcell/version', stemcell_version)
