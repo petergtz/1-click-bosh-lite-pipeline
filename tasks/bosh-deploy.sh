e5ccd80b7208ea2da892ec63d73c11e84e537fec
Norman Sutorius <norman.sutorius@de.ibm.com>
2018-02-20 13:49:47 +0100

    Detect stemcell version used in use-compiled-releases ops file
    [#154842976]
    
    Signed-off-by: Steffen Uhlig <Steffen.Uhlig@de.ibm.com>

--- tasks/bosh-deploy.sh
+++ tasks/bosh-deploy.sh
@@ -2,5 +2,5 @@
 
 . 1-click/tasks/bosh-login.sh
 
-bosh2 upload-stemcell stemcell/stemcell.tgz
+bosh2 upload-stemcell $(cat stemcell/source)
 bosh2 -n -d cf deploy state/environments/softlayer/director/$BOSH_LITE_NAME/cf-deployment/manifest.yml
