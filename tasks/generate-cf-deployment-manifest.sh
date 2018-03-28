2294d8ecf326715dea00db8c4d1ae5b7dc69f630
Alexander Egurnov <speaker92@gmail.com>
2018-03-28 14:18:10 +0200

    Allow to customize cf-deployment
    Signed-off-by: Roberto Jimenez Sanchez <jszroberto@gmail.com>

--- tasks/generate-cf-deployment-manifest.sh
+++ tasks/generate-cf-deployment-manifest.sh
@@ -11,7 +11,7 @@ bosh2 interpolate cf-deployment/cf-deployment.yml \
     -o cf-deployment/operations/use-compiled-releases.yml \
     -o cf-deployment/operations/experimental/use-bosh-dns.yml \
     -o 1-click/operations/add-dns-alias-internal-public-access-entry.yml \
-    ${OPERATIONS} ${VARIABLES} \
+    ${EXTRA_ARGS} \
     > state/environments/softlayer/director/$BOSH_LITE_NAME/cf-deployment/manifest.yml
 
 commit_if_changed=$(readlink -f 1-click/tasks/commit-if-changed.sh)
@@ -22,4 +22,3 @@ pushd state/environments/softlayer/director/$BOSH_LITE_NAME/cf-deployment/
 popd
 
 cp -a state/. out-state
-
